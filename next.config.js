71d0bc5f589c00ecbfb17673028ff3673f40b6f7
Wasim Ahmed <wasim.ahmed00@icloud.com>
2025-05-05 04:45:02 +0100

    next.config ts -> .js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  devIndicators: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
