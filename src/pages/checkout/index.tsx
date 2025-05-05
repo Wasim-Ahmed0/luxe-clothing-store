1fe9d7ab0a06f1b3da7d1a4e20f6d77410bd7061
Wasim Ahmed <wasim.ahmed00@icloud.com>
2025-05-05 04:12:38 +0100

    build error

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useRouter }        from "next/router";
-import "../../../styles/animaions.css";
+import "../../../styles/animations.css";
 
 import Navbar           from "@/components/layout/Navbar"
 import Footer           from "@/components/layout/Footer"
